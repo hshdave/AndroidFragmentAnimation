0bfcebc0c3903a515acf895d56b577226897ae7a
MadReza <rezmaza@gmail.com>
2017-09-04 19:52:15 -0400

    Simple Fragment Animator added for Fragment B

--- app/src/main/java/com/example/rez/fragmentanimation/MainActivity.java
+++ app/src/main/java/com/example/rez/fragmentanimation/MainActivity.java
@@ -34,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
     public void GoToBClicked(View v)
     {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);       //Simple Transition choices: Fade, Open, Close
+        transaction.setCustomAnimations(R.animator.slide_left, R.animator.slide_right);
+                        //Creating 2 animatiosn using the Animator
+                        //We change the property X of both.
+                        //First will exit the Fragment A with a slide to Right
+                        //Then it will enter the new Fragment with Slide Left
         transaction.replace(R.id.fragContainer, bFragment);
         transaction.commit();
     }
