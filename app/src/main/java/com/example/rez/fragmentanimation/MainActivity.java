843e386d7ff2b2a0e017b1a9b40d943e67e14ed6
MadReza <rezmaza@gmail.com>
2017-09-04 18:27:01 -0400

    Added Fragment B and 2 images. Added Simple Switch between Fragments.

--- app/src/main/java/com/example/rez/fragmentanimation/MainActivity.java
+++ app/src/main/java/com/example/rez/fragmentanimation/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.rez.fragmentanimation;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    AFragment aFragment;
+    BFragment bFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aFragment = new AFragment();
+        bFragment = new BFragment();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(R.id.fragContainer, aFragment);
+        transaction.commit();
+    }
+
+    public void GoToAClicked(View v)
+    {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragContainer, aFragment);
+        transaction.commit();
+    }
+
+    public void GoToBClicked(View v)
+    {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragContainer, bFragment);
+        transaction.commit();
     }
 }
